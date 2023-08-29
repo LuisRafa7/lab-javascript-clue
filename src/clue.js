becba5bcefb8ae5a30c5afe8cc28a11551aeb3c1
LuisRafa7 <140761199+LuisRafa7@users.noreply.github.com>
2023-08-29 23:08:37 +0200

    Add files via upload

--- src/clue.js
+++ src/clue.js
@@ -2,15 +2,37 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+const suspectsArray = [
+    { firstName: 'Luis', lastName: 'Henriques', occupation: 'dentist', age: 33, description: '123', image: 'photo1', color: 'azul' },
+    { firstName: 'Celina', lastName: 'Ferreira', occupation: 'TO', age: 33, description: '234', image: 'photo2', color: 'cor-de-rosa' }, 
+    { firstName: 'Jose', lastName: 'Henriques', occupation: 'TOC', age: 33, description: '345', image: 'photo3', color: 'laranja' },
+    { firstName: 'Gabriela', lastName: 'Lemos', occupation: 'Estudante', age: 9, description: '456', image: 'photo4', color: 'amarelo' },
+    { firstName: 'Sofia', lastName: 'Henriques', occupation: '???', age: 21, description: '567', image: 'photo5', color: 'preto' },
+    { firstName: 'Julio', lastName: 'Rocha', occupation: '???', age: 27, description: '678', image: 'photo6', color: 'roxo' }
+];
 
 // Rooms Array
 
-const roomsArray = [];
+const roomsArray = [
+    { name: 'room1'},
+    { name: 'room2'},
+    { name: 'room3'},
+    { name: 'room4'},
+    { name: 'room5'},
+    { name: 'room6'},
+    { name: 'room7'},
+    { name: 'room8'},
+    { name: 'room9'},
+    { name: 'room10'},
+    { name: 'room11'},
+    { name: 'room12'},
+    { name: 'room13'},
+    { name: 'room14'},
+    { name: 'room15'},];
 
 // Weapons Array
 
-const weaponsArray = [];
+const weaponsArray = ['Faca', 'Espingarda', 'Navalha', 'Caçadeira', 'Nerf', 'Garfo'];
 
 
 // ITERATION 2
